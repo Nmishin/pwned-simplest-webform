b242ae2d57cf1ed69bd8b74457f1b325954661f8
Nickolay Mishin <nmishin@griddynamics.com>
2018-03-21 17:12:40 +0300

    Run server in https
    Add generating self-signed certificate and use it for https connection

--- gopass.go
+++ gopass.go
@@ -2,19 +2,11 @@ package main
 
 import (
   "github.com/bmizerany/pat"
+  "github.com/kabukky/httpscerts"
   "html/template"
   "log"
   "net/http"
 )
-
-func main() {
-  mux := pat.New()
-  mux.Get("/", http.HandlerFunc(index))
-  mux.Post("/", http.HandlerFunc(send))
-
-  log.Println("Listening...")
-  http.ListenAndServe(":8080", mux)
-}
 
 func index(w http.ResponseWriter, r *http.Request) {
   render(w, "index.html", nil)
@@ -40,3 +32,26 @@ func render(w http.ResponseWriter, filename string, data interface{}) {
   }
 }
 
+func CheckAndGenCerts() {
+  err := httpscerts.Check("cert.pem", "key.pem")
+  if err != nil {
+    log.Println("Regenerate certificates")
+    err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:8443")
+      if err != nil {
+        log.Fatal("ERROR: Can't generate certificates")
+      }
+   }
+}
+
+func main() {
+  CheckAndGenCerts()
+  mux := pat.New()
+  mux.Get("/", http.HandlerFunc(index))
+  mux.Post("/", http.HandlerFunc(send))
+
+  log.Println("Listening 127.0.0.1:8443...")
+  err := http.ListenAndServeTLS("127.0.0.1:8443", "cert.pem", "key.pem", mux)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
